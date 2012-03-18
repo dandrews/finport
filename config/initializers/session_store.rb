8d48624dab06184dd5587791595baf1faffec26d
Rob Lund <robert.s.lund@gmail.com>
2012-03-18 17:17:18 -0400

    Started a basic rails 3.2 app off of my simple auth template

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Listrhino::Application.config.session_store :cookie_store, key: '_listrhino_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Listrhino::Application.config.session_store :active_record_store
