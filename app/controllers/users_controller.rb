6d5f000e449e2c4a446f853cbb2b3a1236fd5c8a
Albert Peng <albert.peng@halogennetwork.com>
2012-03-21 08:53:30 -0700

    Prettify news feed route, haml-ize signup/login pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to home_news_feed_path, :notice => "Signed up!"
+      redirect_to news_feed_path, :notice => "Signed up!"
     else
       render "new"
     end
