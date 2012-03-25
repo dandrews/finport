fb7e0fe06b9d594499866c9035b1d21d11eaeaaf
Rob Lund <robert.s.lund@gmail.com>
2012-03-25 09:17:44 -0600

    Cleaned out old precompiled assets. Heroku should be doing this as part of the slug compiliation now.

--- public/assets/refresh-e23adc66862b35f9bda616bde5bb7065.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".read-more-link").on("click",function(){$(this).hide(),$(".read-more-text").slideDown()}),$(".read-less-link").on("click",function(){$(".read-more-link").show(),$(".read-more-text").slideUp()})});
